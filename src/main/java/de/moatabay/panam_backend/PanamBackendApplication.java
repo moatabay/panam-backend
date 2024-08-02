e501cfff66046de62ed1cad6ec82a3543838a947
moatabay <mohamed.atabay@outlook.com>
2024-08-02 14:40:36 +0200

    init2

---
+++ src/main/java/de/moatabay/panam_backend/PanamBackendApplication.java
@@ -0,0 +1,13 @@
+package de.moatabay.panam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanamBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/moatabay/panam_backend/PanamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.moatabay.panam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
